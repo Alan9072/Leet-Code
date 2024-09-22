f809489ae8966c059f1b4f44e05248de821df052
Alan Joy <147591098+Alan9072@users.noreply.github.com>
2024-09-22 17:02:14 +0530

    Update Two Sum.java

--- Two Sum.java
+++ Two Sum.java
@@ -1,4 +1,32 @@
 class Solution {
+     int val,val2;
+    public int[] twoSum(int[] nums, int target) {
+        HashSet<Integer> set = new HashSet<>();
+        int[] arr = new int[2];
+        int pos = 0;
+       
+        for(int i=0;i<nums.length;i++){
+            val = nums[i];
+            val2 = target - val;
+            if(!set.contains(val2)){
+                set.add(val);
+            }
+            else{
+                break;
+            }
+        }
+        for(int i=0;i<nums.length;i++){
+
+            if(nums[i] == val || nums[i] == val2){
+                arr[pos] = i;
+                pos++;
+            }
+        }
+        return arr;
+    }
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+class Solution {
     public int[] twoSum(int[] nums, int target) 
     {
         for (int i = 0; i < nums.length; i++) 
