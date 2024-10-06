77b230d46f047352c02ed17ff60f1ff9d1781c91
Alan Joy <147591098+Alan9072@users.noreply.github.com>
2024-10-06 21:19:15 +0530

    Update Search a 2D Matrix.java

--- Search a 2D Matrix.java
+++ Search a 2D Matrix.java
@@ -3,14 +3,14 @@ class Solution {
 
         int left = 0;
         int right = matrix.length - 1;
-        int mid = 0;
+        int rowmid = 0;
         while(left <= right){
-            mid = ( left + right )/2;
-            if(target < matrix[mid][0]){
-                right = mid - 1;
+            rowmid = ( left + right )/2;
+            if(target < matrix[rowmid][0]){
+                right = rowmid - 1;
             }
-            else if(target > matrix[mid][0] && target > matrix[mid][matrix[0].length - 1]){
-                left = mid + 1;
+            else if(target > matrix[rowmid][0] && target > matrix[rowmid][matrix[0].length - 1]){
+                left = rowmid + 1;
             }
             else{
                 break;
@@ -19,13 +19,13 @@ class Solution {
 
 
         left = 0;
-        right = matrix[mid].length - 1;
+        right = matrix[rowmid].length - 1;
         while(left <= right){
             int middle = (left + right)/2;
-            if(target > matrix[mid][middle]){
+            if(target > matrix[rowmid][middle]){
                 left = middle + 1;
             }
-            else if(target < matrix[mid][middle]){
+            else if(target < matrix[rowmid][middle]){
                 right = middle - 1;
             }
             else{
