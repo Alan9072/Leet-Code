56539a9d350063b8784a5f561c96719159d22f00
Alan Joy <147591098+Alan9072@users.noreply.github.com>
2024-10-13 16:38:06 +0530

    Update Reorder List.java

--- Reorder List.java
+++ Reorder List.java
@@ -65,3 +65,54 @@ class Solution {
         }
     }
 }
+///////////////////////////////////////////////////////FAST AND SLOW POINTERS////////////////////////////////////////////////////////////////////////////////////////////////
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode part2 = slow.next;
+        slow.next = null;
+
+        ListNode prev = null;
+        ListNode curr = part2;
+        ListNode after = null;
+        while (curr != null) {
+            after = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = after;
+        }
+
+        ListNode pointer = head;
+        ListNode p1 = head;
+        ListNode p2 = prev;
+        while (pointer != null) {
+            p1 = pointer.next;
+            pointer.next = p2;
+            pointer = pointer.next;
+
+            if (pointer != null) {
+                p2 = pointer.next;
+                pointer.next = p1;
+                pointer = pointer.next;
+            }
+
+        }
+
+    }
+}
