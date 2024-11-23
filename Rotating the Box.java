7eeede72d88d212ff6c4fa14751924beceb828f9
Alan Joy <147591098+Alan9072@users.noreply.github.com>
2024-11-23 08:07:05 +0530

    Create Rotating the Box.java

---
+++ Rotating the Box.java
@@ -0,0 +1,33 @@
+class Solution {
+    public char[][] rotateTheBox(char[][] box) {
+        char[][] result = new char[box[0].length][box.length];
+        for(int i=0;i<box.length;i++){
+            for(int j=box[0].length - 1;j>=0;j--){
+                if(box[i][j] == '.' && j>0){
+                    int left = j;
+  
+                    while(left > 0 ){
+                        left --;
+                        if(box[i][left] =='*'){
+                            j = left;
+                            break;
+                        }
+                        else if(box[i][left] == '#'){
+                            char temp = box[i][j];
+                            box[i][j] = box[i][left];
+                            box[i][left] = temp;
+                            j--;
+
+                        }
+                    }
+                }
+            }
+        }
+        for(int i=0;i<result.length;i++){
+            for(int j=0;j<result[0].length;j++){
+                result[i][j] = box[box.length-1-j][i];
+            }
+        }
+       return result;
+    }
+}
