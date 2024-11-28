8b67cf1d02374fafcde9af3a0789980758882631
Alan Joy <147591098+Alan9072@users.noreply.github.com>
2024-11-28 20:05:29 +0530

    Update Single Number.java

--- Single Number.java
+++ Single Number.java
@@ -1,24 +1,34 @@
 class Solution {
-    public int singleNumber(int[] nums) 
-    {
-        int element=0;
-        int flag;
-        for(int i=0;i<nums.length;i++)
-            {   flag=0;
-                element = nums[i];
-                for(int j=0;j<nums.length;j++)
-                    {
-                        if(nums[j]==element)
-                        {
-                            flag++;
-                        }
-                    }
-                if(flag == 1)
-                {
-                    return element;
-                }
-            }
-        return element;
+    public int singleNumber(int[] nums) {
+        int rslt = 0;
+        for(int i=0;i<nums.length;i++){
+            rslt = rslt ^ nums[i];
+        }
+        return rslt;
     }
-    
 }
+
+// class Solution {
+//     public int singleNumber(int[] nums) 
+//     {
+//         int element=0;
+//         int flag;
+//         for(int i=0;i<nums.length;i++)
+//             {   flag=0;
+//                 element = nums[i];
+//                 for(int j=0;j<nums.length;j++)
+//                     {
+//                         if(nums[j]==element)
+//                         {
+//                             flag++;
+//                         }
+//                     }
+//                 if(flag == 1)
+//                 {
+//                     return element;
+//                 }
+//             }
+//         return element;
+//     }
+    
+// }
