01903938fbc0a0636938c532adc10de3ef074b44
Alan Joy <147591098+Alan9072@users.noreply.github.com>
2024-12-29 12:58:25 +0530

    Update Fibonacci Number.java

--- Fibonacci Number.java
+++ Fibonacci Number.java
@@ -11,7 +11,7 @@ class Solution {
     }
 }
 ////////////////////////////////////////////////////////////
-///TOP - DOWN // with memoization
+///TOP - DOWN  recursion// with memoization
 class Solution {
     public int fib(int n) {
         int[] dp = new int[n + 1];
@@ -27,5 +27,39 @@ class Solution {
 
         dp[n] = fibonacci(n - 1,dp) + fibonacci(n - 2,dp);
         return dp[n];
+    }
+}
+/////////////////////////////////////////////////////////////////
+////Bottom - Up // tabulation
+class Solution {
+    public int fib(int n) {
+        if(n == 0)return 0;
+        if(n == 1)return 1;
+        int dp[] = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i=2;i<dp.length;i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
+}
+/////////////////////////////////////////////////////////////////
+///bottom - up with space O(1);
+class Solution {
+    public int fib(int n) {
+        if(n == 0)return 0;
+        if(n == 1)return 1;
+        int sol = 0;
+
+        int prev2 = 0;
+        int prev = 1;
+        for(int i=2;i<=n;i++){
+            int curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
+            if(i == n)sol = curr;
+        }
+        return sol;
     }
 }
