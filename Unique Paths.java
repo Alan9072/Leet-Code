9f210c1f6e393980895940164820c817ca5028e1
Alan Joy <147591098+Alan9072@users.noreply.github.com>
2025-01-16 16:25:03 +0530

    Update Unique Paths.java

--- Unique Paths.java
+++ Unique Paths.java
@@ -27,3 +27,30 @@ class Solution {
         return right + bottom;
     }
 }
+
+/////////////////////Tabulation ////////////////////////
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n]; 
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i == 0 && j == 0){
+                    dp[i][j] = 1;
+                }else{
+                    int up = 0;
+                    if( i - 1  >= 0){
+                        up = dp[i - 1][j];
+                    } 
+                    int left = 0;
+                    if( j - 1  >= 0){
+                        left = dp[i][j - 1];
+                    }
+
+                    dp[i][j] = left + up;
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
+}
