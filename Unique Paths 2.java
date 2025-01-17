1b31a2fc7c2c2c693cf7e1943aae73fdb9595ef3
Alan Joy <147591098+Alan9072@users.noreply.github.com>
2025-01-17 11:05:23 +0530

    Update Unique Paths 2.java

--- Unique Paths 2.java
+++ Unique Paths 2.java
@@ -28,3 +28,35 @@ class Solution {
         return right + bottom;
     }
 }
+
+//////////////////////Tabulation//////////////////////////
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int rowLen = obstacleGrid.length;
+        int colLen = obstacleGrid[0].length;
+        int dp[][] = new int[rowLen][colLen];
+
+        if (obstacleGrid[0][0] == 1) {
+            return 0;
+        }
+
+        for (int i = 0; i < rowLen; i++) {
+            for (int j = 0; j < colLen; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                }
+                else if(obstacleGrid[i][j] == 1){
+                    dp[i][j] = 0;
+                }else{
+                    if (i > 0) {
+                        dp[i][j] += dp[i - 1][j];
+                    }
+                    if (j > 0) {
+                        dp[i][j] += dp[i][j - 1];
+                    }
+                }
+            }
+        }
+        return dp[rowLen - 1][colLen - 1];
+    }
+}
