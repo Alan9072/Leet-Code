5ba877c19d7a4d3ed58c84d8d616139678dac2c0
Alan Joy <147591098+Alan9072@users.noreply.github.com>
2025-01-17 11:45:26 +0530

    Update Minimum Path Sum.java

--- Minimum Path Sum.java
+++ Minimum Path Sum.java
@@ -25,3 +25,35 @@ class Solution {
         return dp[i][j];
     }
 }
+
+/////////////////////////////Tabulation ///////////////////////////////
+class Solution {
+ 
+    public int minPathSum(int[][] grid) {
+        
+        int[][] dp = new int[grid.length][grid[0].length];
+        for(int i=0;i<grid.length;i++){
+            Arrays.fill(dp[i],-1);
+        }
+
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(i == 0 && j == 0){
+                    dp[i][j] = grid[i][j];
+                }else{
+                    int top = Integer.MAX_VALUE;
+                    int left = Integer.MAX_VALUE;
+                    if(i > 0){
+                         top = dp[i - 1][j];
+                    }
+                    if(j > 0){
+                         left = dp[i][j - 1];
+                    }
+                    dp[i][j] = grid[i][j] +  Math.min(top,left);
+                }
+            }
+        }
+        return dp[grid.length - 1][grid[0].length - 1];
+        
+    }
+}
