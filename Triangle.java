0c16014f4833eb8fe9f22dc10a213dc98d41c680
Alan Joy <147591098+Alan9072@users.noreply.github.com>
2025-01-26 14:23:59 +0530

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -19,3 +19,41 @@ class Solution {
         }
     }
 } 
+///////////////////////////////////Recursion ///////////////////////////////////////////////
+class Solution {
+    int min = Integer.MAX_VALUE;
+    public int minimumTotal(List<List<Integer>> triangle) {
+        List<List<Integer>> dp= new ArrayList<>();
+        for(int i=0;i<triangle.size();i++){
+            List<Integer> level = new ArrayList<>();
+            for(int j=0;j<triangle.get(i).size();j++){
+                level.add(Integer.MAX_VALUE);
+            }
+            dp.add(level);
+        }
+
+        int sum = 0;
+        return find(triangle,0,0,sum,dp);
+        
+    }
+    public int find(List<List<Integer>> triangle,int level,int index,int sum,List<List<Integer>> dp){
+        if(level == triangle.size()){
+            return Integer.MAX_VALUE;
+        }
+        if(dp.get(level).get(index) != Integer.MAX_VALUE){
+            return dp.get(level).get(index);
+        }
+         
+        int down = find(triangle,level + 1,index,sum,dp);
+        int angled = find(triangle,level + 1,index + 1,sum,dp);
+
+        if(down == Integer.MAX_VALUE && angled == Integer.MAX_VALUE){
+            dp.get(level).set(index, triangle.get(level).get(index));
+            return triangle.get(level).get(index);
+        }
+        
+        int val = Math.min(triangle.get(level).get(index) + down , triangle.get(level).get(index) + angled);
+        dp.get(level).set(index, val);
+        return val;
+    }
+}
