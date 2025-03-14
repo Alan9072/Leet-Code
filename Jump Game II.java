e1dbca000c97854143eee7f0c5cff4e3637d627d
Alan Joy <147591098+Alan9072@users.noreply.github.com>
2025-03-14 16:56:18 +0530

    Create Jump Game II.java

---
+++ Jump Game II.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int jump(int[] nums) {
+        int[] dummy = new int[nums.length];
+        Arrays.fill(dummy, -1);
+        return target(nums, 0, dummy);
+    }
+
+    public int target(int[] nums, int start, int[] dummy) {
+        if (start >= nums.length - 1) {
+            return 0;
+        }
+
+        if (dummy[start] != -1) return dummy[start];
+
+        int possibleMin = Integer.MAX_VALUE;
+        for (int i = start + 1; i <= Math.min(start + nums[start], nums.length - 1); i++) { 
+            possibleMin = Math.min(target(nums, i, dummy), possibleMin);
+        }
+
+        dummy[start] = possibleMin == Integer.MAX_VALUE ? possibleMin : possibleMin + 1;
+        return dummy[start];
+    }
+}
