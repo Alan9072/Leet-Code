deded37f176824c7ada1e2e2f65b60cbff96e559
Alan Joy <147591098+Alan9072@users.noreply.github.com>
2025-04-12 20:56:59 +0530

    Create Remove Covered Intervals.java

---
+++ Remove Covered Intervals.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int removeCoveredIntervals(int[][] intervals) {
+        
+        int count = 0;
+        Arrays.sort(intervals, (a, b) -> {
+            if (a[0] != b[0])
+                return a[0] - b[0];
+            return b[1] - a[1];
+        });
+
+        int[] prev = intervals[0];
+        for (int i = 1; i < intervals.length; i++) {
+            int[] curr = intervals[i];
+            if (curr[0] >= prev[0] && curr[1] <= prev[1]){
+                count++;
+            }else{
+                prev = curr;
+            }
+        }
+        return intervals.length - count;
+    }
+}
