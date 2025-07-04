2be221ef536c09af9305f83e51cb9ea3c2af3082
Alan Joy <147591098+Alan9072@users.noreply.github.com>
2025-07-05 00:41:53 +0530

    Update Find the K-th Character in String Game II.java

--- Find the K-th Character in String Game II.java
+++ Find the K-th Character in String Game II.java
@@ -1,32 +1,34 @@
-import java.math.BigInteger;
-
 class Solution {
     public char kthCharacter(long k, int[] operations) {
-        BigInteger two = BigInteger.valueOf(2);
-        BigInteger val = two.pow(operations.length);
-        
-        int total = rec(val, BigInteger.valueOf(k), operations, two, operations.length - 1);
+        long val = 1;
+        int idx = 0;
+        while (val < k) {
+            val *= 2;
+            idx++;
+        }
+                
+        int total = rec(val, k, operations,idx - 1);
         int realAdd = total % 26;
-        return (char)('a' + realAdd);
+        return (char) ('a' + realAdd);
     }
-    
-    public int rec(BigInteger val, BigInteger k, int[] operations, BigInteger two, int pos) {
-        
+
+    public int rec(long val, long k, int[] operations, int pos) {
+
         if (pos < 0) {
             return 0;
         }
-        
-        BigInteger result = val.divide(two);
-        
-        if (k.compareTo(result) > 0) {
-            BigInteger rem = k.subtract(result);
+
+        long result = val / 2;
+
+        if (k > result) {
+            long rem = k - result;
             if (operations[pos] == 1) {
-                return 1 + rec(result, rem, operations, two, pos - 1);
+                return 1 + rec(result, rem, operations, pos - 1);
             } else {
-                return rec(result, rem, operations, two, pos - 1);
+                return rec(result, rem, operations, pos - 1);
             }
         } else {
-            return rec(result, k, operations, two, pos - 1);
+            return rec(result, k, operations, pos - 1);
         }
     }
 }
