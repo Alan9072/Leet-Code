71c648b7a397107b9db3e4aefc87955f39917491
Alan Joy <147591098+Alan9072@users.noreply.github.com>
2025-11-23 10:36:01 +0530

    Create Linked List in Binary Tree.java

---
+++ Linked List in Binary Tree.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        if(root == null) return false;
+
+        if(root.val == head.val){
+            if(linkdfs(root,head)) return true;
+        }
+
+        return isSubPath(head,root.left) || isSubPath(head,root.right);  
+    }
+    
+    public boolean linkdfs(TreeNode root,ListNode head){
+
+        if(head != null && root == null)return false;
+        if(head == null)return true;
+
+        if(root.val == head.val){
+            return linkdfs(root.left,head.next) || linkdfs(root.right,head.next);
+        }
+
+        return false; 
+    }
+}
